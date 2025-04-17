bdc46b65d1c11f0e9b0b2220864402a2a57fb8b9
powersagitar <114868133+powersagitar@users.noreply.github.com>
2025-04-17 12:10:10 -0400

    chore: init commit

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+});
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+});
+
+export const metadata: Metadata = {
+  title: "SE 30' Class Profile",
+  description: 'University of Waterloo Software Engineering 2030 Class Profile',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,24 @@
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center">
+      <h1 className="text-center font-extrabold">
+        University of Waterloo Software Engineering 2030 Class Profile
+      </h1>
+
+      <p>Site under construction.</p>
+
+      <p>
+        SE 30&apos; Discord server:{' '}
+        <Link
+          href="https://discord.gg/nw4N2NNBuE"
+          className="text-bold underline"
+        >
+          nw4N2NNBuE
+        </Link>
+        .
+      </p>
+    </div>
+  );
+}
