8af3809c70d044ec1f37b30daf25d16f9b8dd4af
powersagitar <114868133+powersagitar@users.noreply.github.com>
2025-04-18 13:37:46 -0400

    refactor: use system fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,6 @@
-import './globals.css';
-import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
 import { Analytics } from '@vercel/analytics/next';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
+import type { Metadata } from 'next';
+import './globals.css';
 
 export const metadata: Metadata = {
   title: "SE 30' Class Profile",
@@ -25,9 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className="antialiased">
         {children}
         <Analytics />
       </body>
