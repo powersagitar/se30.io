dbe9317db3a4c603c2a159098c82dc713a2a61dd
powersagitar <114868133+powersagitar@users.noreply.github.com>
2025-04-18 23:07:17 -0400

    refactor: add a bit extra spacing between title and waterloo

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main>
       <article>
-        <header className="mt-auto mb-6 flex flex-col gap-6">
+        <header className="mt-auto mb-6 flex flex-col gap-8">
           <h1 className="text-9xl font-black">
             Software Engineering
             <br />
