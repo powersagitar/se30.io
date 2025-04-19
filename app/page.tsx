d7b3c15bf9dc8bbc846f969f58fb32f2db10757c
powersagitar <114868133+powersagitar@users.noreply.github.com>
2025-04-19 11:32:48 -0400

    refactor: fix nonsemantic html tags

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,16 @@ export default function Home() {
   return (
     <main>
       <article>
-        <header className="mt-auto mb-6 flex flex-col gap-8">
-          <h1 className="text-9xl font-black">
-            Software Engineering
-            <br />
-            2030 Class Profile
-          </h1>
+        <header>
+          <hgroup className="mt-auto mb-6 flex flex-col gap-8">
+            <h1 className="text-9xl font-black">
+              Software Engineering
+              <br />
+              2030 Class Profile
+            </h1>
 
-          <h2 className="text-5xl font-extrabold">University of Waterloo</h2>
+            <p className="text-5xl font-extrabold">University of Waterloo</p>
+          </hgroup>
         </header>
       </article>
     </main>
