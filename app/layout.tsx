4b4c555c21406c8ed174b556ce798361ace4607b
powersagitar <114868133+powersagitar@users.noreply.github.com>
2025-04-18 21:28:10 -0400

    refactor: move components to `components/layout`

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
-import Footer from '@/components/footer';
-import Header from '@/components/header';
+import Footer from '@/components/layout/footer';
+import Header from '@/components/layout/header';
 import { Analytics } from '@vercel/analytics/next';
 import type { Metadata } from 'next';
 import { ThemeProvider } from 'next-themes';
--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
-import { Button } from './ui/button';
-import { Separator } from './ui/separator';
+import { Button } from '../ui/button';
+import { Separator } from '../ui/separator';
 
 export default function Footer() {
   return (
