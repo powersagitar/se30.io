5587781e13baa58eb41b7003681418a62807e673
powersagitar <114868133+powersagitar@users.noreply.github.com>
2025-04-23 09:34:56 -0400

    refactor: my ocd got triggered

--- flags.ts
+++ flags.ts
@@ -3,10 +3,6 @@ import { flag } from 'flags/next';
 export const displaySectionOurCohort = flag({
   key: 'display-section-our-cohort',
   decide() {
-    if (process.env.NODE_ENV === 'development') {
-      return true;
-    } else {
-      return false;
-    }
+    return process.env.NODE_ENV === 'development';
   },
 });
